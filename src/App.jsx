7c0185c83110f577b79ef7b22a5ed35e3f2ffc8e
dev-nirob1 <dev-nirob1@gmail.com>
2023-04-09 10:23:50 +0600

    setting environment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      hello from assignment
+    </div>
+  )
+}
+
+export default App
