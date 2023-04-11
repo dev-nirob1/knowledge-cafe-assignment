ed80a40267f20908941d8f37f9d9ce44ea7bb25b
dev-nirob1 <dev-nirob1@gmail.com>
2023-04-11 14:10:16 +0600

    header complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   return (
     <div className="App">
-      hello from assignment
-      i am a developer
+      <Header></Header>
     </div>
   )
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <header>
+            <nav>
+                <h1>IdeaFountain</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Blog</li>
+                    <li>Categories</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </nav>
+            <hr/>
+        </header>
+    );
+};
+
+export default Header;
