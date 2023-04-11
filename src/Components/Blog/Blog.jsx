64bc23a9c29f4861cbc06ed051e3dd48eef61a3b
dev-nirob1 <dev-nirob1@gmail.com>
2023-04-11 15:22:55 +0600

    blog added

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -4,11 +4,31 @@ import './Blog.css';
 const Blog = (props) => {
     const {
         title, author, authorImg, publishedIn, timeToRead
-        , titleImg
+        , titleImg, hashTags
+
     } = props.blog;
+    console.log(props.blog)
     return (
-        <div>
-            <h3>name: {author}</h3>
+        <div className='blog'>
+            <img src={titleImg} alt="title image" />
+            <div className='author-section'>
+                <div className='image-date'>
+                    <img src={authorImg} alt="author image" />
+                    <div>
+                        <h5>{author}</h5>
+                        <p>{publishedIn} (4 days ago) </p>
+                    </div>
+                </div>
+                <div>
+                    <p>{timeToRead} min read <button>bb</button></p>
+                </div>
+            </div>
+            <h1>{title}</h1>
+            <div className='hashtags'>
+                <p>{hashTags[0]}</p>
+                <p>{hashTags[1]}</p>
+            </div>
+            <button className='button'>Mark as Read</button>
         </div>
     );
 };
--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -3,22 +3,22 @@ import './Body.css'
 import Blog from '../Blog/Blog';
 const Body = () => {
     const [blogs, setBlogs] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('data.json')
-        .then(res => res.json())
-        .then(data => setBlogs(data))
-    },[]);
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, []);
     return (
         <div>
             <div className="blogs-container">
                 <div className="blog">
-       {
-        blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
-       }
+                    {
+                        blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                    }
 
                 </div>
                 <div className="cart">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam esse ad suscipit voluptate officiis sed veritatis exercitationem eveniet laudantium voluptas dolore distinctio rerum, ducimus magni alias quo ea doloribus.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam esse ad suscipit voluptate officiis sed veritatis exercitationem eveniet laudantium voluptas dolore distinctio rerum, ducimus magni alias quo ea doloribus.
                 </div>
             </div>
         </div>
